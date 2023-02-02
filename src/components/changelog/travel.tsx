8ff88aecab603efad7c83db3c865aa6567ea29dc
Tom Bakker <tom.bakker92@gmail.com>
2023-02-02 19:48:28 +0100

    Update travel layout

--- src/components/changelog/travel.tsx
+++ src/components/changelog/travel.tsx
@@ -9,19 +9,18 @@ type Props = {
 export default function Travel({ country, city, createdAt }: Props) {
   return (
     <li>
-      <div className='absolute -left-5 flex h-10 w-10 items-center justify-center rounded-full bg-lime-100 ring-8 ring-gray-50 dark:bg-lime-900 dark:ring-gray-800'>
-        ✈️
-      </div>
+      <div className='absolute -left-5 flex h-10 w-10 items-center justify-center rounded-full bg-lime-100 text-lime-600 ring-8 ring-gray-50 dark:bg-lime-900 dark:ring-gray-800'>✈️</div>
       <div className='ml-10 flex-grow pt-2'>
-        <div className='flex items-center space-x-2 text-base'>
-          <h2>Traveled to <span className="font-semibold">{city}, {country}</span></h2>
-          <span className='text-sm font-normal text-gray-400'>
+        <h2>
+          Traveled to{' '}
+          <span className="text-gray-900">{city}, {country}</span>
+          <span className='text-sm font-normal text-gray-400 ml-2'>
             <TimeAgo
               date={new Date(createdAt)}
               timeStyle='twitter-minute-now'
             />
           </span>
-        </div>
+        </h2>
       </div>
     </li>
   );
